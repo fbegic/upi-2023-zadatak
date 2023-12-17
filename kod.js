f7b426d6356be968cd42046ff467412ea6de6c1a
abilic2 <146930808+abilic2@users.noreply.github.com>
2023-12-17 16:27:59 +0100

    Add files via upload

--- kod.js
+++ kod.js
@@ -1,12 +1,36 @@
-//ZA Desert
-// const sql=require("msnodesqlv8");
 
-// const connestionString="putanja do baze";
+const { JSDOM } = require('jsdom');
+const dom = new JSDOM('<!DOCTYPE html><html><body></body></html>');
 
-// const query="ime svoje tablice iz baze kojoj uzimate podatke";
-// sql.query(connestionString,query,(err,rows)=>{
-//   console.log(rows);
-// })
+// Emulirajte globalni objekt "window" i "document"
+global.window = dom.window;
+global.document = dom.window.document;
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('mojaBaza.db');
+
+// Kreiranje tablice
+db.serialize(() => {
+  db.run('CREATE TABLE IF NOT EXISTS namjernice (id INTEGER PRIMARY KEY, naziv TEXT, cijena INT,kolicina INT)');
+
+  // Dodavanje podataka u tablicu
+  const statement = db.prepare('INSERT INTO namjernice (naziv,cijena,kolicina) VALUES (?, ?, ?)');
+  statement.run('Jabuke', 34, 10);
+  statement.run('Jastog', 33, 15);
+  statement.finalize();
+
+  // Dohvaćanje podataka iz tablice
+  db.each('SELECT id, naziv, cijena, kolicina FROM namjernice', (err, row) => {
+    if (err) {
+      console.error(err.message);
+    } else {
+      console.log(row.id, row.naziv, row.cijena, row.kolicina);
+    }
+  });
+});
+
+// Zatvaranje baze podataka nakon izvršavanja upita
+db.close();
 document.addEventListener('DOMContentLoaded', function() 
  {
    var opcijeDesertaDiv = document.getElementById('opcijeDeserta');
